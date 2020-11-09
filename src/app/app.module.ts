9aedc718c008313fadc3398b7a47c60be7bf061a
tharakatk <tharakatk@gmail.com>
2020-11-09 11:05:39 +0530

    Initials

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { APP_INITIALIZER, LOCALE_ID, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { M3OdinModule } from '@infor-up/m3-odin-angular';
+import { SohoComponentsModule } from 'ids-enterprise-ng'; // TODO Consider only importing individual SoHo modules in production
+import { AppComponent } from './app.component';
+
+@NgModule({
+   declarations: [
+      AppComponent
+   ],
+   imports: [
+      BrowserModule,
+      FormsModule,
+      SohoComponentsModule,
+      M3OdinModule
+   ],
+   providers: [
+      {
+         provide: LOCALE_ID,
+         useValue: 'en-US',
+      },
+      {
+         provide: APP_INITIALIZER,
+         multi: true,
+         useFactory: (locale: string) => () => {
+            Soho.Locale.culturesPath = 'assets/ids-enterprise/js/cultures/';
+            Soho.Locale.set(locale);
+         },
+         deps: [LOCALE_ID],
+      }
+   ],
+   bootstrap: [AppComponent]
+})
+export class AppModule { }
